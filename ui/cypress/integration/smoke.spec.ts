6d9c56fa3b152fbff046a1e5a6e58eb6d97c6fea
uidoyen <nasiba.ansari@gmail.com>
2021-07-05 19:05:30 +0530

    update smoke test query

--- ui/cypress/integration/smoke.spec.ts
+++ ui/cypress/integration/smoke.spec.ts
@@ -4,8 +4,4 @@ context('I am on the debezium-ui homepage', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-  it('Check if able to connect backend and get connectors', () => {
-    cy.intercept('/api/connectors/1').as('getConnector')
-    cy.wait('@getConnector', { timeout: 50000 }).its('response.statusCode').should('eq', 200)
-  })
 })
